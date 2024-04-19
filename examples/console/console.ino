c66f32d79207535df208e17804f5d09e95b79460
Bojan Jurca <42709677+BojanJurca@users.noreply.github.com>
2024-04-19 22:09:38 +0200

    Add files via upload

--- examples/console/console.ino
+++ examples/console/console.ino
@@ -1,8 +1,9 @@
 #include "std/console.hpp"
 
+
 void setup () {
     // Serial (console) initialization
-    cinit (true);                                             // two optional arguments: bool waitForSerial = false, unsigned int serialSpeed = 115200
+    cinit (true);                                             // 3 optional arguments: bool waitForSerial = false, unsigned int serialSpeed = 115200, unsigned int waitAfterSerial = 1000
 
     // Serial (console) input
     int i;
@@ -11,6 +12,11 @@ void setup () {
 
     // Serial (console) output
     cout << "you entered " << i << endl;
+
+    // Try something more complicated
+    String s = " cmbination";
+    char c [] = " of";
+    cout << "Let's try" << s << c << " different text data types\r\n";
 }
 
 void loop () {
