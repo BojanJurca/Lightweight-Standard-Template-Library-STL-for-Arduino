50d924a9a6c13360ccb40a44b5b03a219f1c6286
Bojan Jurca <42709677+BojanJurca@users.noreply.github.com>
2025-11-26 22:22:12 +0100

    Add files via upload

--- examples/algorithm/algorithm.ino
+++ examples/algorithm/algorithm.ino
@@ -1,4 +1,5 @@
 #include <ostream.hpp>      // cout instance for Arduino
+#include <array.hpp>        // arrays for Arduino
 #include <list.hpp>         // single linked lists with error handling and ability to use PSRAM for Arduino
 #include <vector.hpp>       // vectors with error handling and ability to use PSRAM for Arduino
 #include <algorithm.hpp>    // find, heap sort for vectors, ... merge sort for lists
@@ -8,19 +9,20 @@ void setup () {
 
     cinit ();                                                   // three optional arguments: bool waitForSerial = false, unsigned int waitAfterSerial = 100 [ms], unsigned int serialSpeed = 115200 (9600 for AVR boards)
 
-    // ----- algorithm functions on arrays -----
+    // ----- algorithm functions on C arrays -----
+    cout << "----- C arrays -----\n";
 
     int Fibonacci [] = { 0, 1, 1, 2, 3, 5, 8, 21, 13 };
     int size = sizeof (Fibonacci) / sizeof (Fibonacci [0]);
  
     // Finding an element
-    auto fa = find (Fibonacci, Fibonacci + size, 13);
-    if (fa != Fibonacci + size)
-        cout << "Found " << *fa << endl;    
+    auto fca = find (Fibonacci, Fibonacci + size, 13);
+    if (fca != Fibonacci + size)
+        cout << "Found " << *fca << endl;    
 
     // Finding min, max element
-    auto ma = max_element (Fibonacci, Fibonacci + size);
-        cout << "Max element in the array = " << *ma << endl;
+    auto mca = max_element (Fibonacci, Fibonacci + size);
+        cout << "Max element in the C array = " << *mca << endl;
 
     // Sorting
     cout << "Heap-sorted: ";
@@ -30,7 +32,28 @@ void setup () {
     cout << endl;
 
 
+    // ----- algorithm functions on arrays -----
+    cout << "----- arrays -----\n";
+
+    array<int, 8> Lucas ( { 2, 1, 3, 4, 7, 11, 18, 29 } );
+
+    // Finding an element
+    auto flc = find (Lucas.begin (), Lucas.end (), 11);
+    if (flc != Lucas.end ())
+        cout << "Found " << *flc << endl;
+
+    // Finding min, max element
+    auto mlc = max_element (Lucas.begin (), Lucas.end ());
+        cout << "Max element in the array = " << *mlc << endl;
+
+    // Sorting
+    cout << "Heap-sorted: ";
+    sort (Lucas.begin (), Lucas.end ());
+    cout << Lucas << endl;
+
+
     // ----- algorithm functions on lists -----
+    cout << "----- lists -----\n";
 
     list<String> Grocery ( { String ("milk"), String ("bread"), String ("salad"), String ("tomatoes"), String ("eggs") } );
  
@@ -50,6 +73,7 @@ void setup () {
 
 
     // ----- algorithm functions on vectors -----
+    cout << "----- vectors -----\n";
 
     vector<char> Direction ( { 'U', 'D', 'L', 'R' } );
 
