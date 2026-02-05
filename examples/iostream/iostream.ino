1857e2c7e5b5d3b4fdbbb989d4fb6b55658f32d1
Bojan Jurca <42709677+BojanJurca@users.noreply.github.com>
2026-02-06 00:04:04 +0100

    Add files via upload

--- examples/iostream/iostream.ino
+++ examples/iostream/iostream.ino
@@ -6,7 +6,7 @@ void setup () {
     cinit (); // three optional arguments: bool waitForSerial = false, unsigned int waitAfterSerial = 100 [ms], unsigned int serialSpeed = 115200 (9600 for AVR boards)
 
     cout << "Please enter integer number. ";
-    int i;
+    int i = 0;
     cin >> i;
     cout << "You entered " << i << "." << endl;
 }
